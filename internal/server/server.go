4b89a58d45cc30afcfbbaa60017b8d6a4ee1e6d2
junsik park <junsik.park@kwic.co.kr>
2026-07-10 06:53:42 +0900

    Align JSON response encoding

--- internal/server/server.go
+++ internal/server/server.go
@@ -2682,9 +2682,7 @@ func writeError(w http.ResponseWriter, status int, message string) {
 func writeJSON(w http.ResponseWriter, status int, value any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	encoder := json.NewEncoder(w)
-	encoder.SetIndent("", "  ")
-	_ = encoder.Encode(value)
+	_ = json.NewEncoder(w).Encode(value)
 }
 
 const maxTailBytes = 1048576
